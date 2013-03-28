c3532c1d4a0bafc37f046cae72c5fab4865a8796
Zach Dyer <zach.dyer@zachdyerdesign.com>
2013-03-28 12:36:42 -0500

    Added a menu for starting point
    This also will handle time passage for doing dishes and

--- Logan and Zach's Game/main.cpp
+++ Logan and Zach's Game/main.cpp
@@ -11,6 +11,7 @@ int main()
     cout << title << endl;
     cout << "1. New Game" << endl;
     cout << "2. Continue" <<endl;
+    cout << "3. Quit" <<endl;
     string input;
     cin >> input;
     system("cls");
@@ -28,25 +29,55 @@ int main()
         cout << "1. Male \n2. Female" << endl;
         cin >> gender;
         if (gender == "1") {
+            system("cls");
             cout << "You're a dude!" << endl;
             gender = "male";
         }
         else if(gender == "2") {
-            cout << "You're a chick!" << endl;
+            system("cls");
+            cout << "Dang, girl, you got it going on!" << endl;
             gender = "female";
             }
-
-        cout << "What a nice house, " << name << "." << endl;
-        cout << "The dishes are piled in the sink. \nThe floor is covered in dog pee and poop. \n1You only have half an hour till work." << endl;
-
-        cout << "Ready to go to Work?" << endl;
+        int minutes = 30;
+        bool dishesDone = false;
+        bool floorClean = false;
+        while(true){
+            cout << "What a nice house, " << name << "." << endl;
+            if(dishesDone == false){
+                cout << "The dishes are piled in the sink." << endl;
+            } else {
+                cout << "The dishes are done." << endl;
+            }
+            if(floorClean == false){
+                cout << "The floor is covered in dog pee and poop." << endl;
+            } else {
+                cout << "The floors are clean and sparkling." << endl;
+            }
+            cout << "You only have " << minutes << " minutes till work." << endl;
+            cout << "1. Do the dishes - 15 min" << endl;
+            cout << "2. Clean floors - 15 min" << endl;
+            cout << "3. Walk to work - 15 min" << endl;
+            cin >> input;
+            if(input == "1" && dishesDone == false){
+                minutes -= 15;
+                dishesDone = true;
+                system("cls");
+            } else if(input == "2" && floorClean == false){
+                minutes -= 15;
+                floorClean = true;
+                system("cls");
+            } else if(input == "3"){
+                minutes -= 15;
+                system("cls");
+                break;
+            } else {
+                system("cls");
+            }
+        }
     }
     else if(input == "2") {
         cout << "Let's get back to business.";
+    } else{
+        return 0;
     }
-
-
-
-
-    return 0;
 }
